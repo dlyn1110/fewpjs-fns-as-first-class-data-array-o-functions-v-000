74f7882b77c72ba1457208d246cb484acad4a1d9
Dani Lyn <dlyn1110@gmail.com>
2020-04-23 20:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function wakeDog(dogName, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
 }
+function leashDog(dogName, dogBreed) {
+  console.log("Leash ${dogName} the ${dogBreed}");
+}
+console.log("Leash Byron the poodle");
+console.log("Walk to the park with Byron the poodle");
+console.log("Throw the frisbee for Byron the poodle");
+console.log("Walk home with Byron the poodle");
+console.log("Unleash Byron the poodle");
